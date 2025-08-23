b949a9c27cefa167c764773e7d0d3f1e2498d0df
Cesar Guadarrama <cesargdm@hey.com>
2025-08-22 22:06:52 -0600

    fix: update Firebase initialization to use modular SDK and add Firebase dependency

---
+++ src/lib/firebase/index.web.ts
@@ -0,0 +1,64 @@
+import { CryptoDigestAlgorithm, digestStringAsync } from 'expo-crypto'
+import {
+	getAnalytics,
+	setAnalyticsCollectionEnabled,
+	setConsent,
+	setUserId,
+	setUserProperties,
+} from 'firebase/analytics'
+
+const algorithm = CryptoDigestAlgorithm.SHA256
+
+export async function enableAnalytics({
+	email,
+	firstName,
+	lastName,
+	phoneNumber,
+	userId,
+}: {
+	email?: string
+	firstName?: string
+	lastName?: string
+	phoneNumber?: string
+	userId?: string
+}) {
+	const analytics = getAnalytics()
+
+	setAnalyticsCollectionEnabled(analytics, true)
+	setConsent({
+		ad_personalization: 'granted',
+		ad_storage: 'granted',
+		ad_user_data: 'granted',
+		analytics_storage: 'granted',
+	})
+
+	const [emailAddressHash, firstNameHash, lastNameHash, phoneNumberHash] =
+		await Promise.all([
+			email ? hash256(email, 'email') : null,
+			firstName ? hash256(firstName) : null,
+			lastName ? hash256(lastName) : null,
+			phoneNumber ? hash256(phoneNumber, 'phone') : null,
+		])
+
+	if (userId) {
+		setUserId(analytics, userId)
+	}
+
+	setUserProperties(analytics, {
+		sha256_email_address: emailAddressHash ?? null,
+		sha256_first_name: firstNameHash ?? null,
+		sha256_last_name: lastNameHash ?? null,
+		sha256_phone_number: phoneNumberHash ?? null,
+	})
+}
+
+export function hash256(value: string, type?: 'email' | 'phone') {
+	if (type === 'email') {
+		return digestStringAsync(algorithm, value.toLowerCase())
+	}
+	if (type === 'phone') {
+		return digestStringAsync(algorithm, `+${value.replaceAll(/\D/g, '')}`)
+	}
+
+	return digestStringAsync(algorithm, value)
+}
--- src/lib/firebase/init.web.ts
+++ src/lib/firebase/init.web.ts
@@ -1,19 +1,6 @@
-import firebase from '@react-native-firebase/app'
-import { MMKV } from 'react-native-mmkv'
-// Before initializing Firebase set the Async Storage implementation
-// that will be used to persist user sessions.
+import { initializeApp } from 'firebase/app'
 
-const firebaseStorage = new MMKV()
-
-const storage = {
-	getItem: (key: string) => firebaseStorage.getString(key),
-	removeItem: (key: string) => firebaseStorage.delete(key),
-	setItem: (key: string, value: string) => firebaseStorage.set(key, value),
-}
-
-firebase.setReactNativeAsyncStorage(storage)
-
-await firebase.initializeApp({
+initializeApp({
 	apiKey: 'AIzaSyCIBdUpvzHyvetsp2p0g8AM8yHob6aYd1E',
 	appId: '1:503502595311:web:5f7dd78115adc82464671a',
 	authDomain: 'tolo-cafe.firebaseapp.com',
