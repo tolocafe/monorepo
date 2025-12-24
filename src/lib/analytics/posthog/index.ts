b7f5ec369bec9a96b6cf3d2805b0e84d17475eea
Cesar Guadarrama <cesargdm@hey.com>
2025-12-24 12:05:45 -0600

    debug: log POSTHOG_API_KEY for verification during development

--- src/lib/analytics/posthog/index.ts
+++ src/lib/analytics/posthog/index.ts
@@ -5,6 +5,8 @@ import { posthogStorage } from './storage'
 const POSTHOG_API_KEY = process.env.EXPO_PUBLIC_POSTHOG_API_KEY as string
 const POSTHOG_HOST = 'https://a.tolo.cafe'
 
+console.log('POSTHOG_API_KEY', POSTHOG_API_KEY)
+
 /**
  * PostHog client instance
  * Configured with custom MMKV storage for optimal performance
