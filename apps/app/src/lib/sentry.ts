14c1b3eef8584bc516016d19fa8098861cf7f3dc
Cesar Guadarrama <cesargdm@hey.com>
2026-02-13 23:13:46 -0600

    fix: update version to 1.5.2 and reorder Sentry integrations

--- apps/app/src/lib/sentry.ts
+++ apps/app/src/lib/sentry.ts
@@ -5,9 +5,9 @@ const dsn = process.env.EXPO_PUBLIC_SENTRY_DSN
 Sentry.init({
 	dsn,
 	integrations: [
-		Sentry.mobileReplayIntegration(),
 		Sentry.feedbackIntegration(),
 		Sentry.screenshotIntegration(),
+		Sentry.mobileReplayIntegration(),
 	],
 	replaysOnErrorSampleRate: 1,
 	replaysSessionSampleRate: 0.1,
